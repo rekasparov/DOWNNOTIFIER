2ec72bad0e281d4d73ea00ff54b9e7d27f3ab468
Proceed-MustafaBurakKayabal <151638396+Proceed-MustafaBurakKayabal@users.noreply.github.com>
2024-04-04 14:24:07 +0300

    #4

---
+++ DOWNNOTIFIER/DOWNNOTIFIER.DataAccessLayer/Concrete/ApplicationDAL.cs
@@ -0,0 +1,21 @@
+﻿using DOWNNOTIFIER.DataAccessLayer.Abstract;
+using DOWNNOTIFIER.Entity;
+using DOWNNOTIFIER.Repository.Abstract;
+using DOWNNOTIFIER.Repository.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOWNNOTIFIER.DataAccessLayer.Concrete
+{
+    public class ApplicationDAL : BaseEFRepository<Application>, IApplicationDAL
+    {
+        public ApplicationDAL(DbContext dbContext)
+            : base(dbContext)
+        {
+        }
+    }
+}
