74924070de4056b23da4cf01463b6f8a4595536f
Proceed-MustafaBurakKayabal <151638396+Proceed-MustafaBurakKayabal@users.noreply.github.com>
2024-04-04 17:50:50 +0300

    #8

---
+++ DOWNNOTIFIER/DOWNNOTIFIER.Extension/ApplicationExtension.cs
@@ -0,0 +1,29 @@
+﻿using DOWNNOTIFIER.DataTransferObject;
+using DOWNNOTIFIER.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOWNNOTIFIER.Extension
+{
+    public static class ApplicationExtension
+    {
+        public static ApplicationDTO ToDTO(this Application entity)
+        {
+            return new ApplicationDTO
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                Url = entity.Url,
+                IntervalTime = entity.IntervalTime,
+                IsActive = entity.IsActive,
+                CreatedBy = entity.CreatedBy,
+                CreatedDate = entity.CreatedDate,
+                UpdatedBy = entity.UpdatedBy,
+                UpdatedDate = entity.UpdatedDate
+            };
+        }
+    }
+}
---
+++ DOWNNOTIFIER/DOWNNOTIFIER.Extension/UserRoleExtension.cs
@@ -0,0 +1,22 @@
+﻿using DOWNNOTIFIER.DataTransferObject;
+using DOWNNOTIFIER.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOWNNOTIFIER.Extension
+{
+    public static class UserRoleExtension
+    {
+        public static UserRoleDTO ToDTO(this UserRole entity)
+        {
+            return new UserRoleDTO
+            {
+                Id = entity.Id,
+                Name = entity.Name
+            };
+        }
+    }
+}
