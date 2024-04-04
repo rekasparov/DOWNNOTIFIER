ea9f08344d58ac7ee0c2084af2e7f5326942f1e9
Proceed-MustafaBurakKayabal <151638396+Proceed-MustafaBurakKayabal@users.noreply.github.com>
2024-04-04 14:18:28 +0300

    #3

---
+++ DOWNNOTIFIER/DOWNNOTIFIER.Repository/Concrete/BaseEFRepository.cs
@@ -0,0 +1,42 @@
+﻿using DOWNNOTIFIER.Repository.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOWNNOTIFIER.Repository.Concrete
+{
+    public class BaseEFRepository<T> : IBaseRepository<T>
+        where T : class, new()
+    {
+        protected readonly DbContext _dbContext;
+        protected BaseEFRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Delete(T entity)
+        {
+            _dbContext.Remove(entity);
+        }
+
+        public void Insert(T entity)
+        {
+            _dbContext.Add(entity);
+        }
+
+        public IQueryable<T> Select(Expression<Func<T, bool>>? predicate = null)
+        {
+            if (predicate == null)
+                return _dbContext.Set<T>().AsQueryable();
+            return _dbContext.Set<T>().Where(predicate);
+        }
+
+        public void Update(T entity)
+        {
+            _dbContext.Update(entity);
+        }
+    }
+}
