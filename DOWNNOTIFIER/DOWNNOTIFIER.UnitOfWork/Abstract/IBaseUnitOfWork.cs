88790d4a48b3b6e41e13eb07df7a856cd4bc9f6e
Proceed-MustafaBurakKayabal <151638396+Proceed-MustafaBurakKayabal@users.noreply.github.com>
2024-04-04 14:33:38 +0300

    #6

---
+++ DOWNNOTIFIER/DOWNNOTIFIER.UnitOfWork/Abstract/IBaseUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using DOWNNOTIFIER.DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOWNNOTIFIER.UnitOfWork.Abstract
+{
+    public interface IBaseUnitOfWork
+    {
+        public IApplicationDAL Application { get; }
+        public IUserDAL User { get; }
+        public IUserRoleDAL UserRole { get; }
+
+        public int SaveChanges();
+    }
+}
